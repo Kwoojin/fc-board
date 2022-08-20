82e23a06459402c19f4ad78ef280b88ead03f698
김우진 <dnwls488@naver.com>
2022-08-20 20:49:35 +0900

    #16 - 게시글 뷰 기본 골격 작성
    컨트롤러, 뷰, 테스트까지 한 번에 적용

--- src/main/java/com/fc/board/controller/ArticleController.java
+++ src/main/java/com/fc/board/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.fc.board.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -16,5 +17,12 @@ public class ArticleController {
     public String articles(ModelMap map) {
         map.addAttribute("articles", List.of());
         return "articles/index";
+    }
+
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId, ModelMap map) {
+        map.addAttribute("article", "article"); // TODO: 구현할 때 여기에 실제 데이터를 넣어줘야 한다
+        map.addAttribute("articleComments", List.of());
+        return "articles/detail";
     }
 }
--- src/test/java/com/fc/board/controller/ArticleControllerTest.java
+++ src/test/java/com/fc/board/controller/ArticleControllerTest.java
@@ -37,7 +37,6 @@ class ArticleControllerTest {
         
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
