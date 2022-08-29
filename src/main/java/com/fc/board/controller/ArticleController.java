616849f2af79d59f067d0f8f559223baa6136031
김우진 <dnwls488@naver.com>
2022-08-29 22:23:00 +0900

    #23 - 페이지네이션 기능을 컨트롤러(게시판 뷰)에 반영
    테스트는 'Page' 클래스를 이용하기 때문에
    하는 김에 페이징과 정렬을 함께 테스트
    나중에 정렬 구현에서 이어서 활용할 전망

--- src/main/java/com/fc/board/controller/ArticleController.java
+++ src/main/java/com/fc/board/controller/ArticleController.java
@@ -4,7 +4,9 @@ import com.fc.board.domain.type.SearchType;
 import com.fc.board.dto.response.ArticleResponse;
 import com.fc.board.dto.response.ArticleWithCommentsResponse;
 import com.fc.board.service.ArticleService;
+import com.fc.board.service.PaginationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -15,12 +17,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
 
     @GetMapping
     public String articles(
@@ -29,7 +34,12 @@ public class ArticleController {
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             ModelMap map
     ) {
-        map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from));
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
+
         return "articles/index";
     }
 
